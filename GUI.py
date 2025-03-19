a0cb2e6d555c1d92d6cd6d3fe1b07ca3fc740352
JimTPRI <309698758@qq.com>
2025-03-19 18:51:05 +0800

    Change theme, add clock, day 17.

--- GUI.py
+++ GUI.py
@@ -1,17 +1,22 @@
 # from CLI import new_todo
 from modules import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("LightBrown3")
+
+clock = sg.Text("", key="clock")
 label = sg.Text("输入 to-do")
 input_box: sg.Input = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("添加")
+add_button = sg.Button("添加", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("编辑")
 complete_button = sg.Button("完成")
 exit_button = sg.Button("退出")
 
-layout = [[label],
+layout = [[clock],
+          [label],
           [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button]]
@@ -20,14 +25,12 @@ window = sg.Window("My Todo App",
                    layout=layout,
                    font=("helvetica", 18))
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "添加":
             if values["todo"] == "" or values["todo"] == "\n":
-                print("Invalid")
+                sg.popup("Invalid", font=("helvetica", 16))
             else:
                 new_todo = values["todo"] + "\n"
                 todos = functions.get_todos()
@@ -38,7 +41,7 @@ while True:
         case "编辑":
             try:
                 if values["todo"] == "" or values["todo"] == "\n":
-                    print("Invalid")
+                    sg.popup("Invalid", font=("helvetica", 16))
                 else:
                     todo_to_edit = values["todos"][0]
                     new_todo = values["todo"] + "\n"
@@ -52,10 +55,10 @@ while True:
                     window["todos"].update(values=todos)
                     window["todo"].update(value="")
             except IndexError:
-                print("No selection")
+                sg.popup("No selection", font=("helvetica", 16))
                 continue
             except ValueError:
-                print("Invalid input")
+                sg.popup("Invalid input", font=("helvetica", 16))
                 continue
         case "完成":
             try:
@@ -66,7 +69,7 @@ while True:
                 window["todos"].update(values=todos)
                 window["todo"].update(value="")
             except IndexError:
-                print("No selection")
+                sg.popup("No selection", font=("helvetica", 16))
                 continue
         case "退出":
             break
@@ -75,7 +78,7 @@ while True:
                 #window["todo"].update(value=values["todos"][0])
                 True
             except IndexError:
-                print("Void")
+                sg.popup("Void", font=("helvetica", 16))
                 continue
         case sg.WIN_CLOSED:
             break
