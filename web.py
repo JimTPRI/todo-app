65f8c861f76fc14f8ca0502d9ce15639ca7e90b7
JimTPRI <309698758@qq.com>
2025-03-20 17:51:53 +0800

    fix web app, day 18.

--- web.py
+++ web.py
@@ -12,8 +12,14 @@ st.title("My Todo App")
 st.subheader("这是一个待办事项应用")
 st.write("提高自己的生产力吧")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="", placeholder="添加待办事项", on_change=add_todo,
               key="new_todo")
